a5bc04c6987bd57b1d30a0ca4d0ea37908723c6a
KYHSGeekCode <jourhyang123@gmail.com>
2021-04-22 22:48:39 +0900

    :bug: SIGSEGV

--- parser.cpp
+++ parser.cpp
@@ -57,6 +57,37 @@ int Parser::parse_expression(vector<Token>::iterator begin, vector<Token>::itera
         }
     }
 
+    tmp = parse_number(begin, end, tmpNode);
+    if (tmp > 0) {
+        pNode = tmpNode;
+        return tmp;
+    }
+
+    tmp = parse_write(begin, end, tmpNode);
+    if (tmp > 0) {
+        pNode = tmpNode;
+        return tmp;
+    }
+
+    shared_ptr<ID> tmpIdNode;
+    tmp = parse_id(begin, end, tmpIdNode);
+    if (tmp > 0) {
+        pNode = tmpIdNode;
+        return tmp;
+    }
+
+    shared_ptr<Add> addNode;
+    tmp = parse_add(begin, end, addNode);
+    if (tmp > 0) {
+        pNode = addNode;
+        return tmp;
+    }
+    tmp = parse_let(begin, end, tmpNode);
+    if (tmp > 0) {
+        pNode = tmpNode;
+        return tmp;
+    }
+
     return 0;
 }
 
@@ -103,13 +134,86 @@ int Parser::parse_id(vector<Token>::iterator begin, vector<Token>::iterator end,
 int Parser::parse_comment(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode) {
     cout << "parse_comment" << endl;
     int cnt = 1;
-    cout << "Begin type :" << begin->get_type() << endl;
     if (begin->get_type() == Token::LCOMMENT) {
         while ((begin + cnt)->get_type() != Token::RCOMMENT) {
             cout << "Comment: " << (begin + cnt)->get_raw() << ";;";
             cnt++;
         }
-        return cnt;
+        cout << endl;
+        return cnt + 1;
+    }
+    return 0;
+}
+
+int Parser::parse_let(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode) {
+    cout << "Parse let" << endl;
+    if (begin->get_type() == Token::LET) {
+        cout << "is let" << endl;
+        shared_ptr<ID> tmp_id_node;
+        int tmp = parse_id(begin + 1, end, tmp_id_node);
+        if (tmp > 0) {
+            cout << "success get id" << endl;
+            if ((begin + 1 + tmp)->get_type() == Token::ASSIGN) {
+                cout << "assign is right" << endl;
+                shared_ptr<Node> valueNode;
+                int tmp2 = parse_expression(begin + 1 + tmp + 1, end, valueNode);
+                if (tmp2 > 0) {
+                    if ((begin + 1 + tmp + 1 + tmp2)->get_type() == Token::IN) {
+                        shared_ptr<Node> thenNode;
+                        int tmp3 = parse_expression(begin + 1 + tmp + 1 + tmp2 + 1, end, thenNode);
+                        if (tmp3 > 0) {
+                            pNode = make_shared<LetV>(tmp_id_node, valueNode, thenNode);
+                            return 1 + tmp + 1 + tmp2 + 1 + tmp3;
+                        }
+                    }
+                }
+            } else {
+                cout << " Not assingg " << endl;
+            }
+        } else if ((begin + 1)->get_type() == Token::PROC) {
+
+        }
+    } else {
+        cout << "Not let" << endl;
+    }
+    return 0;
+}
+
+int Parser::parse_number(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode) {
+    cout << "parse_number" << endl;
+    if (begin->get_type() == Token::NUM) {
+        string r = begin->get_raw();
+        int val = stoi(r);
+        pNode = make_shared<Num>(val);
+        return 1;
+    }
+    return 0;
+}
+
+int Parser::parse_write(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode) {
+    if (begin->get_type() == Token::WRITE) {
+        shared_ptr<Node> tmpNode;
+        int tmp = parse_expression(begin + 1, end, tmpNode);
+        if (tmp > 0) {
+            pNode = make_shared<Write>(tmpNode);
+            return 1 + tmp;
+        }
+    }
+    return 0;
+}
+
+int Parser::parse_add(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Add> pNode) {
+    shared_ptr<Node> tmpNode1;
+    int tmp = parse_expression(begin, end, tmpNode1);
+    if (tmp > 0) {
+        if ((begin + tmp)->get_type() == Token::PLUS) {
+            shared_ptr<Node> tmpNode2;
+            int tmp2 = parse_expression(begin + tmp + 1, end, tmpNode2);
+            if (tmp2 > 0) {
+                pNode = make_shared<Add>(tmpNode1, tmpNode2);
+                return tmp + 1 + tmp2;
+            }
+        }
     }
     return 0;
 }
--- parser.h
+++ parser.h
@@ -25,7 +25,7 @@ public:
         WHILE,
         READ,
         WRITE,
-        LETIN,
+        LETV,
         LETPROC,
         CALL,
         CALLR,
@@ -82,8 +82,51 @@ public:
     Sequence(shared_ptr<Node> e1, shared_ptr<Node> e2) : Node(Node::SEQ), e1(e1), e2(e2) {}
 
     shared_ptr<Node> e1, e2;
+
+    friend ostream &operator<<(ostream &os, const Sequence &sequence) {
+        os << static_cast<const Node &>(sequence) << " e1: " << sequence.e1 << " e2: " << sequence.e2;
+        return os;
+    }
 };
 
+
+class LetV : public Node {
+public:
+    LetV(shared_ptr<ID> to, shared_ptr<Node> what, shared_ptr<Node> then) : Node(Node::LETV), to(to), what(what),
+                                                                            then(then) {}
+
+    shared_ptr<ID> to;
+
+    friend ostream &operator<<(ostream &os, const LetV &v) {
+        os << static_cast<const Node &>(v) << " to: " << v.to << " what: " << v.what << " then: " << v.then;
+        return os;
+    }
+
+    shared_ptr<Node> what;
+    shared_ptr<Node> then;
+};
+
+
+class Num : public Node {
+public:
+    explicit Num(int value) : Node(Node::NUM), value(value) {}
+
+    int value;
+};
+
+class Write : public Node {
+public:
+    explicit Write(shared_ptr<Node> v) : Node(Node::WRITE), v(v) {}
+
+    shared_ptr<Node> v;
+};
+
+class Add : public Node {
+public:
+    Add(shared_ptr<Node> v1, shared_ptr<Node> v2) : Node(Node::ADD), v1(v1), v2(v2) {}
+
+    shared_ptr<Node> v1, v2;
+};
 
 class Parser {
 public:
@@ -106,6 +149,14 @@ private:
     int parse_id(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<ID> pNode);
 
     int parse_comment(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode);
+
+    int parse_let(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode);
+
+    int parse_number(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode);
+
+    int parse_write(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Node> pNode);
+
+    int parse_add(vector<Token>::iterator begin, vector<Token>::iterator end, shared_ptr<Add> pNode);
 };
 
 
